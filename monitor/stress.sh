449ed3bfad657477e45db3c1a44eb2d905f497f8
Enol Fernandez <enol.fernandez@egi.eu>
2018-07-19 14:56:57 +0100

    Script to do some stress testing

---
+++ monitor/stress.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Simple script to stress test a jupyter deployment by spawning
+# a set of notebooks in parallel
+
+MAX_SERVERS=40
+
+OUTDIR=$(mktemp -d)
+for USER in $(seq -w $MAX_SERVERS); do
+    SINGLE_EXECUTION=TRUE DEBUG=TRUE STATUS_FILE=$OUTDIR/status$USER.json JUPYTERHUB_USER=stress$USER python monitor.py 2>&1 | tee $OUTDIR/monitor_$USER.log &
+done
+wait
+echo "Results at $OUTDIR"
+
